86a68f7400de6e34c5885a87a565a6d8433b584b
brenio <72355208+brenioks@users.noreply.github.com>
2026-01-13 14:27:47 -0300

    Reformatação no WorldManager
    - typei explicitamente as variaveis do world.gd;
    - esqueci de excluir o antigo label_mission.gd no outro commit;

--- SCRIPT/world.gd
+++ SCRIPT/world.gd
@@ -1,13 +1,12 @@
-class_name WorldManager
-extends Node
+class_name WorldManager extends Node
 
 # ---------------- ESTADOS ----------------
 var state: String = "IDLE"
 
-var limite_inimigos := [10, 20, 50, 15, 1]
-var inimigos_derrotados = GameData.inimigos_derrotados
+var limite_inimigos: PackedInt32Array = [10, 20, 50, 15, 1]
+var inimigos_derrotados: int = GameData.inimigos_derrotados
 
-var iniciou_combat = GameData.iniciou_combat
+#var iniciou_combat = GameData.iniciou_combat		# Não usado
 
 @export var player: Node2D
 @export var enemies_container: Node
@@ -18,64 +17,66 @@ var iniciou_combat = GameData.iniciou_combat
 @export var npc_parent: Node2D
   
 # ---------------- READY ----------------
-func _ready():
+func _ready() -> void:
 	hud_tempo.visible = false
 	countdown.stop()
 	start_idle()  # Começa no estado IDLE
 
 # ---------------- ESTADOS ----------------
-func start_idle():
+func print_state(sufix: String = "") -> void:
+	print("World ESTADO: %s%s" % [state, " - " + sufix if not sufix.is_empty() else ""])
+
+func start_idle() -> void:
 	state = "IDLE"
+	print_state()
 	hud_tempo.visible = false
 	countdown.stop()
 	spawnar_npc()
-	print("Estado: IDLE")
 
-func start_reading():
+func start_reading() -> void:
 	if state != "IDLE":
 		return
 	state = "READING"
+	print_state()
 	hud_tempo.visible = false
-	print("Estado: READING")
 
-func start_combat():
+func start_combat() -> void:
 	if state != "READING":
 		return
 	state = "COMBAT"
+	print_state("Combate iniciado")
 	inimigos_derrotados = 0
 	countdown.start(randi_range(2,4))
-	print("Estado: COMBAT - Combate iniciado") 
 	GameData.iniciou_combat = true
 
 # ---------------- NPC ----------------
-func spawnar_npc():
+func spawnar_npc() -> void:
 	if is_instance_valid(npc_parent):
 		npc_parent.call_deferred("queue_free")
-
+	
 	npc_parent = npc_scene.instantiate()
 	call_deferred("add_child", npc_parent)
-
-
+	
 	# Define referência para este WorldManager
-	if npc_parent.has_node("."):
+	if npc_parent.has_node("."):				# b: Pq ele verifica se tem ele mesmo?
 		var npc_script = npc_parent.get_child(0)
 		npc_script.world = self
-
-	var posicoes = [
-		Vector2(244,105),
-		Vector2(620,188),
-		Vector2(244,105),
-		Vector2(620,188),
-		Vector2(776,414)
+	
+	const posicoes: Array[Vector2] = [
+		Vector2(244, 105),
+		Vector2(620, 188),
+		Vector2(244, 105),
+		Vector2(620, 188),
+		Vector2(776, 414)
 	]
 	npc_parent.position = posicoes[GameData.missao_atual]
 	npc_parent.visible = true
-
+	
 	# Começa a leitura quando o NPC aparece
 	start_reading()
 
 # ---------------- HUD ----------------
-func _physics_process(_delta):
+func _physics_process(_delta: float) -> void:
 	match state:
 		"IDLE", "READING":
 			hud_tempo.visible = false
@@ -84,12 +85,12 @@ func _physics_process(_delta):
 			hud_tempo.text = str(floori(countdown.time_left))
 
 # ---------------- LEITURA ----------------
-func leitura_concluida():
+func leitura_concluida() -> void:
 	start_combat()
 	GameData.pegou_missao = true
 
 # ---------------- INIMIGOS ----------------
-func _on_countdown_timeout():
+func _on_countdown_timeout() -> void:
 	countdown.start(randi_range(0,5))
 	if state != "COMBAT":
 		return
@@ -102,13 +103,13 @@ func _on_countdown_timeout():
 	enemies_container.add_child(enemy)
 	enemy.died.connect(_on_enemy_died)
 
-func _on_enemy_died():
+func _on_enemy_died() -> void:
 	inimigos_derrotados += 1
 	if inimigos_derrotados >= limite_inimigos[GameData.missao_atual]:
 		finalizar_missao()
 
 # ---------------- FINAL ----------------
-func finalizar_missao():
+func finalizar_missao() -> void:
 	print("Missão concluída")
 	start_idle()
 	GameData.iniciou_combat = false
