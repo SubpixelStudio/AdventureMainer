619f91cab91c9ce0de0bef1e1250471004916a9c
brenio <72355208+brenioks@users.noreply.github.com>
2026-01-13 15:03:52 -0300

    Reformatação no Player
    - typei explicitamente as variaveis do player.gd;
    - separei os exports em grupos;
    - mudei o anim e o attack_area pra serem @onready;
    - diminui a velocidade das animações do Player e aumentei no "icone" dele do HUD;
    - mudei todos os strings literals para string normais por consistencia;

--- SCRIPT/player.gd
+++ SCRIPT/player.gd
@@ -1,37 +1,44 @@
 extends CharacterBody2D
 
+const NORMAL_ANIM_SPEED: float = .6
+const ATTACK_ANIM_SPEED: float = 1
+
+@export_group("Properties")
 @export var speed: float = 200
 @export var attack_cooldown: float = 0.4
 @export var damage: int = 10
 @export var max_life: int = 100
-@export var max_mana:int = 200
-@export var anim: AnimationPlayer
-@export var attack_area: Area2D
-@export var health:ProgressBar
-@export var mana:ProgressBar
-@export var npc:Node2D
-@export var world:WorldManager
-@export var animation:AnimationPlayer
+@export var max_mana: int = 200
+@export_group("Nodes")
+@export var health: ProgressBar
+@export var mana: ProgressBar
+@export var npc: Node2D
+@export var world: WorldManager
+@export var animation: AnimationPlayer
+
+@onready var anim: AnimationPlayer = $Anim
+@onready var attack_area: Area2D = $AttackArea
+
 var life: int
 var power: int
 var last_direction: Vector2 = Vector2.DOWN
 
-var can_attack := true
-var is_attacking := false
-var attack_index := 1
-var is_dead := false
-var is_attacked := false
+var can_attack: bool = true
+var is_attacking: bool = false
+var attack_index: int = 1
+var is_dead: bool = false
+var is_attacked: bool = false
 
 # ------------------------------
 # SISTEMA DE ALVO
 # ------------------------------
 var enemies: Array[Node2D] = []
-var target_index := 0
+var target_index: int = 0
 var current_target: Node2D = null
 
 # -------------------------------------------------
 
-func _ready():
+func _ready() -> void:
 	life = max_life
 	power = max_mana
 	attack_area.monitoring = false
@@ -41,7 +48,7 @@ func _ready():
 
 # -------------------------------------------------
 
-func _physics_process(delta):
+func _physics_process(_delta: float) -> void:
 	health.value = life
 	health.max_value = max_life
 	mana.value = power
@@ -65,7 +72,7 @@ func _physics_process(delta):
 # INIMIGOS / ALVO
 # -------------------------------------------------
 
-func update_enemy_list():
+func update_enemy_list() -> void:
 	enemies.clear()
 
 	for enemy in get_tree().get_nodes_in_group("enemies"):
@@ -80,7 +87,7 @@ func update_enemy_list():
 	current_target = enemies[target_index]
 
 
-func handle_target_selection():
+func handle_target_selection() -> void:
 	if Input.is_action_just_pressed("enemy"):
 		if enemies.is_empty():
 			current_target = null
@@ -103,7 +110,7 @@ func get_target_direction() -> Vector2:
 # ATUALIZAÇÃO DO HITBOX (MIRA)
 # -------------------------------------------------
 
-func update_attack_area():
+func update_attack_area() -> void:
 	var shape := attack_area.get_node("Polygon")
 	var direction := get_target_direction()
 
@@ -115,7 +122,7 @@ func update_attack_area():
 # MOVIMENTO
 # -------------------------------------------------
 
-func handle_movement():
+func handle_movement() -> void:
 	var input_vector := Input.get_vector("A", "D", "W", "S")
 	velocity = input_vector * speed
 
@@ -129,12 +136,12 @@ func handle_movement():
 # ATAQUE
 # -------------------------------------------------
 
-func handle_attack_input():
+func handle_attack_input() -> void:
 	if Input.is_action_pressed("attack") and can_attack and power > 0:
 		attack()
 		power -= 10
 
-func attack():
+func attack() -> void:
 	can_attack = false
 	is_attacking = true
 
@@ -153,7 +160,7 @@ func attack():
 	can_attack = true
 
 
-func _on_attack_area_body_entered(body):
+func _on_attack_area_body_entered(body: Node2D) -> void:
 	if body.has_method("take_damage"):
 		body.take_damage(damage)
 
@@ -161,7 +168,7 @@ func _on_attack_area_body_entered(body):
 # VIDA / MORTE
 # -------------------------------------------------
 
-func take_damage(amount: int):
+func take_damage(amount: int) -> void:
 	if is_dead:
 		return
 
@@ -171,7 +178,7 @@ func take_damage(amount: int):
 	if life <= 0:
 		die()
 
-func die():
+func die() -> void:
 	is_dead = true
 	velocity = Vector2.ZERO
 	print("Player morreu")
@@ -184,29 +191,33 @@ func die():
 # ANIMAÇÕES
 # -------------------------------------------------
 
-func play_walk_animation():
+func play_walk_animation() -> void:
+	anim.speed_scale = NORMAL_ANIM_SPEED
 	play_directional_animation("walk")
-	animation.play('Walking')
+	animation.play("Walking")
 
-func play_idle_animation():
+func play_idle_animation() -> void:
+	anim.speed_scale = NORMAL_ANIM_SPEED
 	play_directional_animation("idle")
-	animation.play('Current')
+	animation.play("Current")
 	if power < max_mana and !is_attacked:
 		power += 1
 	if life < max_life and !is_attacked:
 		life += 1
 
-func play_attack_animation():
-	animation.play('Current')
+func play_attack_animation() -> void:
+	anim.speed_scale = ATTACK_ANIM_SPEED
 	play_directional_animation("attack", true)
+	animation.play("Current")
 
-func play_directional_animation(prefix: String, alternate := false):
-	var anim_name := ""
-
+func play_directional_animation(prefix: String, alternate: bool = false) -> void:
+	var sufix: String
+	
 	if abs(last_direction.x) > abs(last_direction.y):
-		anim_name = prefix + ("_right" if last_direction.x > 0 else "_left")
+		sufix = ("_right" if last_direction.x > 0 else "_left")
 	else:
-		anim_name = prefix + ("_down" if last_direction.y > 0 else "_up")
+		sufix = ("_down" if last_direction.y > 0 else "_up")
+	var anim_name: String = prefix + sufix
 
 	if alternate:
 		anim_name += str(attack_index)
