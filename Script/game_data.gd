fc3eebce0757bd025cf43cba7e31f269eb44cfb5
brenio <72355208+brenioks@users.noreply.github.com>
2026-01-23 13:46:20 -0300

    Revert "[VT] BUG FIX"
    This reverts commit fc567767ee05dfbaee8bbed99853a62f5c2d2405.

--- Script/game_data.gd
+++ Script/game_data.gd
@@ -6,7 +6,6 @@ var pegou_missao = false
 var missao_atual = 0
 var limite_de_inimigos = [10, 20, 50, 15, 1]
 
-var has_dead = false
 # cheats pra test
 var jogador_imortal = false
 var autoiniciar_missao = false
--- Script/player.gd
+++ Script/player.gd
@@ -314,7 +314,6 @@ func take_damage(amount: int, attacker: CharacterBody2D = null) -> void:
 
 func die() -> void:
 	is_dead = true
-	GameData.has_dead = true
 	velocity = Vector2.ZERO
 	print("Player morreu")
 	
